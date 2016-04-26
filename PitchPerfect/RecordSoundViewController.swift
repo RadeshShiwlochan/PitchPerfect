06cfbd60086e7c524d2ad47b4652728fb4352a14
Radesh Shiwlochan <radesh0430@gmail.com>
2016-04-26 00:37:59 -0400

    change name of view controller, added code for recording audio

--- PitchPerfect/RecordSoundViewController.swift
+++ PitchPerfect/RecordSoundViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class RecordSoundViewController: UIViewController {
 
     @IBOutlet weak var pressToRecordButton: UIButton!
     @IBOutlet weak var PressToRecordLabel: UILabel!
     @IBOutlet weak var pressToStopButton: UIButton!
+    
+    var audioRecorder:AVAudioRecorder!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +31,20 @@ class ViewController: UIViewController {
         pressToRecordButton.enabled = false
         pressToStopButton.enabled = true
         PressToRecordLabel.text = "Recording"
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask,true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        print(filePath)
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        
+        try! audioRecorder = AVAudioRecorder(URL: filePath!, settings:[:])
+        audioRecorder.meteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
     }
 
     @IBAction func stopButtonPressed(sender: UIButton) {
